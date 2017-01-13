b380e35d495dfd052eba6c4c5cca80a156263dda
Jeremy Rambo <jeremy.rambo.muqn@statefarm.com>
2017-01-13 11:16:30 -0600

    Added more sample to the coercion sample

--- javascript/es6/samples/variables/coercion.js
+++ javascript/es6/samples/variables/coercion.js
@@ -1,4 +1,6 @@
 
 if (true && {} && 1 && 'true') {
-  console.log('truthy statements:', true === true, {} == true, 1 == true, 'true' == true);
+  console.log('all coerced truthy statements');
+  console.log('\tyet not true statements:', true === true, {} == true, 1 == true, 'true' == true);
+  console.log('\tcertainly not type true statements:', true === true, {} === true, 1 === true, 'true' === true);
 }
